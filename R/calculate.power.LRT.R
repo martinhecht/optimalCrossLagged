2af56774a4c9ef1007be5c8e410f63bf42faf185
martin <martin.hecht@uni-tuebingen.de>
2022-04-22 00:45:35 +0200

    v0.0.10 more lean T-specific F_diff calculations in calculate.power.LRT

--- R/calculate.power.LRT.R
+++ R/calculate.power.LRT.R
@@ -1,4 +1,5 @@
 ## Changelog:
+# MH 0.0.3 2022-04-22: more lean T-specific F_diff calculations
 # MH 0.0.2 2022-02-25: added caching of T-specific F_diff calculations
 # MA 0.0.1 2022-02-24: initial programming
 
@@ -23,15 +24,18 @@ calculate.power.LRT <- function(alpha, N, timepoints, n_ov, n_process,
   # if env exists
   if( !is.null( pwrLRT.env ) ){
 
-		# get previous F_diff_values if exist
-		if( "F_diff_values" %in% ls( envir=pwrLRT.env ) ) {
-			F_diff_values <- get( "F_diff_values", envir=pwrLRT.env )
+		# get previous F_diff_list if exist
+		if( "F_diff_list" %in% ls( envir=pwrLRT.env ) ) {
+			F_diff_list <- get( "F_diff_list", envir=pwrLRT.env )
 			
 			# if existing, try to get F_diff for timepoint
-			if( as.character(timepoints) %in% colnames(F_diff_values) ){
-				F_diff_values. <- F_diff_values[ , as.character(timepoints) ]
-				# create original F_diff structure
-				F_diff <- list( "values" = F_diff_values., "df"=get( "df", envir=pwrLRT.env ) )
+			if( as.character(timepoints) %in% names(F_diff_list) ){
+				F_diff <- F_diff_list[[ as.character(timepoints) ]]
+				
+				if( verbose ){
+					cat( paste0( "F_diff for time point ", timepoints," recovered from cache", "\n" ) )
+					flush.console()
+				}
 			}
 		}
   }
@@ -50,31 +54,38 @@ calculate.power.LRT <- function(alpha, N, timepoints, n_ov, n_process,
 	  
 	  # if env exists (=caching is desired)
 	  if( !is.null( pwrLRT.env ) ){
-			# matrix
-			F_diff_matrix <- matrix( F_diff$values, nrow=length( F_diff$values ), ncol=1 )
-			rownames( F_diff_matrix ) <- names( F_diff$values )
-			colnames( F_diff_matrix ) <- as.character(timepoints)
+			
+			# F_diff_list
+			F_diff_list_current_timepoint <- list( F_diff )
+			names( F_diff_list_current_timepoint ) <- as.character(timepoints)
 		
-			# attach F_diff to cumulative F_diff_values matrix
-			if( !"F_diff_values" %in% ls( envir=pwrLRT.env ) ) {
-					F_diff_values <- F_diff_matrix
+			# attach F_diff_list_current_timepoint to F_diff_list
+			if( !"F_diff_list" %in% ls( envir=pwrLRT.env ) ) {
+					F_diff_list <- F_diff_list_current_timepoint
 			} else {
-					F_diff_values <- cbind( F_diff_values, F_diff_matrix )
+					F_diff_list <- c( F_diff_list, F_diff_list_current_timepoint )
 			}
 			
 			# assign into environment
-			assign( "F_diff_values", F_diff_values, pos = pwrLRT.env,
+			assign( "F_diff_list", F_diff_list, pos = pwrLRT.env,
 													inherits = FALSE, immediate=TRUE)
-			assign( "df", F_diff$df, pos = pwrLRT.env, inherits = FALSE, immediate=TRUE)
 			
 			if( verbose ){
-				cat( paste0( "new F_diff values added", "\n" ) )
-				cat( paste0( "F_diff values in cache: ", "\n" ) )
-				print( F_diff_values )
-				cat( paste0( "df in cache: ", F_diff$df, "\n" ) )
+				cat( paste0( "new F_diff for time point ", timepoints," added", "\n" ) )
+				cat( paste0( "F_diff in cache: ", "\n" ) )
+				print( F_diff_list )
 				flush.console()
 			}
 	  }
+  }
+
+  # print F_diff values and df for power calculation
+  if( verbose ){
+		cat( paste0( "used for power calculation:\n" ) )
+		cat( paste0( "   F_diff values: ", paste( F_diff$values, collapse=", " ), "\n" ) )
+		cat( paste0( "   df: ", F_diff$df, "\n" ) )
+		cat( paste0( "   N: ", N, "\n" ) )
+		flush.console()
   }
 
   # Critical value
